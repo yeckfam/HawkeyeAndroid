13d50f5159164adfcbf34af56b700197816ba7fd
Fan Yang <fanyang@Fans-Macintosh.local>
2015-11-25 14:57:20 -0800

    Add another way of video view

---
+++ app/src/main/java/com/hawkidrone/httpwww/hawkeye/VideoViewActivity.java
@@ -0,0 +1,71 @@
+package com.hawkidrone.httpwww.hawkeye;
+
+/**
+ * Created by fanyang on 11/25/15.
+ */
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoViewActivity extends ActionBarActivity {
+
+    // Declare variables
+    ProgressDialog pDialog;
+    VideoView videoview;
+
+    // Insert your Video URL
+    String VideoURL = "http://192.168.42.87:8080/stream/video.h264";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
+        setContentView(R.layout.videoview);
+        // Find your VideoView in your video_main.xml layout
+        videoview = (VideoView) findViewById(R.id.VideoView);
+        // Execute StreamVideo AsyncTask
+
+        // Create a progressbar
+        pDialog = new ProgressDialog(VideoViewActivity.this);
+        // Set progressbar title
+       // pDialog.setTitle("Android Video Streaming Tutorial");
+        // Set progressbar message
+        pDialog.setMessage("Buffering...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        // Show progressbar
+        pDialog.show();
+
+        try {
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(
+                    VideoViewActivity.this);
+            mediacontroller.setAnchorView(videoview);
+            // Get the URL from String VideoURL
+            Uri video = Uri.parse(VideoURL);
+            videoview.setMediaController(mediacontroller);
+            videoview.setVideoURI(video);
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoview.requestFocus();
+        videoview.setOnPreparedListener(new OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                pDialog.dismiss();
+                videoview.start();
+            }
+        });
+
+    }
+}
